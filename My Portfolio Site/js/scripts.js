9ac53d797381dd02a0ef884428944721e55dbcfc
hwchen424 <102466540+hwchen424@users.noreply.github.com>
2022-04-12 03:43:20 -0400

    1.7 -2.3 Submission

---
+++ My Portfolio Site/js/scripts.js
@@ -0,0 +1,10 @@
+console.log("Oh man, here we go again")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
